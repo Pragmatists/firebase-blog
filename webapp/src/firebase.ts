f0db359a7a6df89ba291406295d3c6fe38e33af2
Krzysztof Rzymkowski <krzysztof.rzymkowski@pragmatists.pl>
2020-01-10 15:13:22 +0100

    initialize firebase

---
+++ webapp/src/firebase.ts
@@ -0,0 +1,12 @@
+import firebase from "firebase/app";
+
+firebase.initializeApp({
+    "projectId": "fir-blog-9304f",
+    "appId": "1:388751691931:web:9752de94c07db91326363e",
+    "databaseURL": "https://fir-blog-9304f.firebaseio.com",
+    "storageBucket": "fir-blog-9304f.appspot.com",
+    "locationId": "europe-west",
+    "apiKey": "AIzaSyDxTIw-McJJ-pd1rqYCryeKWwkUaTzutlQ",
+    "authDomain": "fir-blog-9304f.firebaseapp.com",
+    "messagingSenderId": "388751691931"
+});
